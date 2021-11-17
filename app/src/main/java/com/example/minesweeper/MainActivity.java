3fdd01e2044e1d4e63109e85259b53eac62b4631
Beomseok <beso1234@naver.com>
2021-11-17 15:34:28 +0900

    메인 클래스(테이블 레이아웃 생성, 마인과 숫자 생성)

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -2,80 +2,126 @@ package com.example.minesweeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.Random;
 
-    boolean flag;
-    boolean mine;
-    int neighMines;
-    static int a;
-    static int flags;
-    static int blocks;
+public class MainActivity extends AppCompatActivity {
+    static TextView textView;
+    boolean mines[][];
+    static BlockButton[][] buttons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ToggleButton togglebutton = findViewById(R.id.toggleButton);
+        TableLayout table = (TableLayout) findViewById(R.id.tableLayout);
 
-        BlockButton[][] buttons = new BlockButton[9][9];
+        mines = createMines(9);
+        buttons = new BlockButton[9][9];
+
+        BlockButton.mineBlocks = mines;
+        BlockButton.buttons = this.buttons;
+        BlockButton.brokenBlocks = new boolean[9][9];
+        BlockButton.mines = 10;
+        BlockButton.blocks = 81;
+
+        // 9*9 블록 생성
+        TableRow.LayoutParams layoutParams =
+                new TableRow.LayoutParams(
+                        TableRow.LayoutParams.WRAP_CONTENT,
+                        TableRow.LayoutParams.WRAP_CONTENT,
+                        1.0f);
+
         for (int i = 0; i < 9; i++) {
-            TableLayout table;
-            table = (TableLayout) findViewById(R.id.tableLayout);
             TableRow tableRow = new TableRow(this);
-            table.addView(tableRow);
             for (int j = 0; j < 9; j++) {
                 buttons[i][j] = new BlockButton(this, i, j);
-                TableRow.LayoutParams layoutParams =
-                        new TableRow.LayoutParams(
-                                TableRow.LayoutParams.WRAP_CONTENT,
-                                TableRow.LayoutParams.WRAP_CONTENT,
-                                1.0f);
                 buttons[i][j].setLayoutParams(layoutParams);
+                if (mines[i][j]) {
+                    buttons[i][j].mine = true;
+                }
                 tableRow.addView(buttons[i][j]);
+            }
+            table.addView(tableRow);
+        }
 
-                buttons[i][j].setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        ((BlockButton) view).toggleFlag2();
-                    }
-                });
+        //재시작 버튼
+        Button restartButton;
+        restartButton = findViewById(R.id.restart);
+        restartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                restart(view);
+            }
+        });
 
+        //우상단 마인개수
+        textView = (TextView) findViewById(R.id.mine);
+        textView.setText("Mines : " + BlockButton.mines);
 
-                togglebutton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        for (int i = 0; i < 9; i++) {
-                            for (int j = 0; j < 9; j++) {
-                                if (isChecked) {
-                                    buttons[i][j].setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View view) {
-                                            ((BlockButton) view).toggleFlag();
-                                        }
-                                    });
-                                } else {
-                                    buttons[i][j].setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View view) {
-                                            ((BlockButton) view).toggleFlag2();
-                                        }
-                                    });
-                                }
-                            }
-                        }
-                    }
-                });
+        //토글 버튼
+        ToggleButton toggleButton;
+        toggleButton = (ToggleButton) findViewById(R.id.togglebutton);
+        //toggle 버튼으로 숫자/깃발 변환 -> boolean으로
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean on) {
+                if (on) { //on
+                    BlockButton.toggleflag = true;
+                } else { //off
+                    BlockButton.toggleflag = false;
+                }
+            }
+        });
+    }
+
+    //게임 재시작 메소드
+    public void restart(View view) {
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
+    }
+
+    //깃발 뽑을 때 우상단 마인 남은 개수
+    public static void putOffFlags() {
+        textView.setText("Mines : " + Integer.toString(BlockButton.mines));
+    }
+
+    //깃발 꼿을 때 우상단 마인 남은 개수
+    public static void putFlags() {
+        textView.setText("Mines : " + Integer.toString(BlockButton.mines));
+    }
+
+    //지뢰 랜덤하게 생성
+    public boolean[][] createMines(int m) {
+        boolean[][] mines = new boolean[m][m];
+        String[] numbers = new String[m + 1];
+
+        Random random = new Random();
+
+        for (int i = 0; i < m + 1; i++) {
+            int x = random.nextInt(m); // row
+            int y = random.nextInt(m); // column
+            if (Arrays.asList(numbers).contains(String.valueOf((x) + (y)))) { //중복이면 건너뛴다
+                i--;
+                continue;
+            } else {
+                numbers[i] = String.valueOf((x) + (y));
+                mines[x][y] = true;
             }
         }
+        return mines;
     }
 }
